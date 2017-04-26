529f6ab655cdfd7f37c1994df5426b9247d0f21c
Andreas Maier <andreas.alexander.maier@cern.ch>
2017-04-26 14:05:35 +0200

    new HexPlot.exe, some adaptations in GUI and output file

--- HexPlot_manual.bat
+++ HexPlot_manual.bat
@@ -1,1 +1,5 @@
 HexPlot.exe -df NONE -pn 0 -i "results\log_v1.txt" -g "hex_positions_HPK_128ch_6inch_Fermilab.txt" -o  "test_Fermilab.pdf" -ys 1 -IV  -p HEX  -select 10  -if SELECTOR:PADNUM:VAL:no:no
+
+
+
+#HexPlot.exe -df NONE -i C:\Users\Administrator\cernbox\HGCAL test results\Data\HPK_6in_135_1052\HPK_6in_135_1052_IV.txt -g C:\Users\Administrator\Desktop\HGCAL_sensor_tests\hex_positions_HPK_128ch_6inch_testcap.txt -o C:\Users\Administrator\cernbox\HGCAL test results\Data\HPK_6in_135_1052\HPK_6in_135_1052_IV.pdf -IV -p SEL -ys 1 -c 28 -if SELECTOR:PADNUM:VAL:no:no:no:no:no:no:no:no:no:no:no:no -pn 0
